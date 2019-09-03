e3e467350acd1853f7e677df0bed536194b3e611
unknown <shahanmny@gmail.com>
2019-09-03 15:04:59 -0400

    refactored layout, more organized, easier to understand

---
+++ gear_analytics/__main__.py
@@ -0,0 +1,9 @@
+from gear_analytics.main_gui import gui
+
+if __name__ == "__main__":
+    gui = gui()
+    gui.find_port()
+    gui.display_img()
+
+
+
---
+++ gear_analytics/settings_gui.py
@@ -0,0 +1,26 @@
+import PySimpleGUI as sg
+
+class settingsGui:
+    def settings(default_thresh, something):
+
+        layout = [[sg.Text('Default Thresh')] +
+                  [sg.InputText(default_thresh, key='default_thresh')],
+                  [sg.Text('Something')] +
+                  [sg.InputText(something, key='something')],
+                  [sg.Button('Reset', button_color = ('white','#bfbfbf'))] +
+                  [sg.Button('Exit', button_color = ('white','#bfbfbf'))]]
+        
+        window = sg.Window('Setting', layout, location=(300,200))
+        while True:
+            event, values = window.Read(timeout=20)
+            if event == 'Reset':
+                window.FindElement('default_thresh').Update(default_thresh)
+                window.FindElement('something').Update(something)
+            elif event is None or event == 'Exit':
+                if values['default_thresh'] == '' or values['default_thresh'] > 255:
+                    values['default_thresh'] = default_thresh
+                if values['something'] == '':
+                    values['something'] = something
+                window.Close()
+                return int(values['default_thresh']), float(values['something'])
+    
