bb012f945792a48f35b13fd3a79e374b04032569
Shahan Mudasir <shahanmny@gmail.com>
2021-06-16 15:11:17 -0400

    Added comments

--- gear_analytics/settings_gui.py
+++ gear_analytics/settings_gui.py
@@ -1,45 +1,59 @@
 import PySimpleGUI as sg
 
 class SettingsGui:
-    def settings(default_thresh, parameter, background_size):
+    """
+    Contains the methods to run the Settings GUI
+    """
+    def settings(default_thresh, background_width):
+        """
+        Runs the Settings GUI, where users can change several options
+
+        Parameters:
+            default_thresh: The current default thresh
+            background_width: The current width of the background frame
+
+        Returns:
+            new_default_thresh: The new default thresh
+            new_background_width: The new background width
+        """
+        #The Setting GUI layout
         layout = [[sg.Text('Default Thresh')] +
                   [sg.InputText(default_thresh, key='default_thresh')],
-                  [sg.Text('Parameter')] +
-                  [sg.InputText(parameter, key='parameter')],
                   [sg.Text('Background Size')] +
-                  [sg.InputText(background_size, key='background_size')] +
+                  [sg.InputText(background_width, key='background_width')] +
                   [sg.Text('Inches')],
                   [sg.Button('Reset', button_color = ('white','#bfbfbf'))] +
                   [sg.Button('Save & Exit', button_color = ('white','#bfbfbf'))]]
         
+        #The Setting GUI window
         window = sg.Window('Setting', layout, location=(300,200))
         
         while True:
+            #Incoming Events from the Setting GUI
             event, values = window.Read(timeout=20)
             
+            #Reset the options
             if event == 'Reset':
                 window.FindElement('default_thresh').Update(230)
-                window.FindElement('parameter').Update(0.008)
-                window.FindElement('background_size').Update(5)
+                window.FindElement('background_width').Update(5)
             
+            #Save and Exit the Setting GUI, by returning the new changes
             elif event == None or event == 'Save & Exit':
                 new_default_thresh = None
-                new_parameter = None
-                new_background_size = None
+                new_background_width = None
 
                 try:
                     if values == None:
                         raise Exception('User Exited') 
                     
                     new_default_thresh = values['default_thresh']
-                    new_parameter = values['parameter']
-                    new_background_size = values['background_size']
+                    new_background_width = values['background_width']
                 
                     window.Close()
                     
-                    return int(new_default_thresh), float(new_parameter), float(new_background_size)
+                    return int(new_default_thresh), float(new_background_width)
                 
                 except:
-                    return default_thresh, parameter, background_size
+                    return default_thresh, background_width
                           
                     
