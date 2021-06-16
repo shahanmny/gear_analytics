534c4896919b8b839f0d08765edc0dd8d1264843
Shahan Mudasir <shahanmny@gmail.com>
2021-06-16 15:10:43 -0400

    Updated Main GUI

--- gear_analytics/main_gui.py
+++ gear_analytics/main_gui.py
@@ -4,58 +4,86 @@ from gear_analytics.gear import Gear
 from gear_analytics.settings_gui import SettingsGui
 
 class Gui:
-    def __init__(self, second_camera):
+    """
+    The Primary GUI
+
+    Attributes:
+    cap: The camera that is used for the video capture
+    second_camera: Whether or not if the second camera should be used
+    background_width: The width of the frame in inches
+    """
+
+    def __init__(self, second_camera, background_width):
+        """
+        The constructor for the GUI class
+        
+        Parameters:
+            cap: The camera that is used for the video capture
+            second_camera: Whether or not if the second camera should be used
+            background_width: The width of the frame in inches
+        """
         self.cap = None
         self.second_camera = second_camera
+        self.background_width = background_width
     
-    #looks through each device index to find a camera
-    def find_cam(self):  
-            working_device = None
-
-            for index in range(100):
-                
-                #index 0 can be the laptop camera
-                if index == 0 and self.second_camera == True:
-                    continue
-                
-                self.cap = cv2.VideoCapture(index)
-                ret, frame = self.cap.read()
-                
-                if self.cap.isOpened() and ret:
-                    working_device = index
-                
-                self.cap.release() 
+    def find_cam(self):
+        """Find the camera"""  
+        working_device = None
+        
+        for index in range(100):
             
-            if working_device == None:
-                print('No Camera Found')
-                exit()
-            self.cap = cv2.VideoCapture(working_device)
-                
+            #If the user wants to use the second camera, then skip the first one
+            if index == 0 and self.second_camera == True:
+                continue
+            
+            #Get the device and capture a image
+            self.cap = cv2.VideoCapture(index)
+            ret, frame = self.cap.read()
+            
+            #Check if the camera works
+            if self.cap.isOpened() and ret:
+                working_device = index
+                break
+            
+            self.cap.release() 
+        
+        #If no camera was found, then exit the program
+        if working_device == None:
+            print('Camera Not Found')
+            exit()
+        
+        #Assign the camera to a object property
+        self.cap = cv2.VideoCapture(working_device)
+            
     def display_gui(self):
+        """Displays and Runs the GUI"""
         sg.theme('LightGray1')
 
-        #default_threshold, parameter and background size can be changed in the settings
+        #default_threshold and background_width can be changed in the settings
         
         #thresh value which is used to classify the pixel values
         #must be between 0 and 255
-        default_thresh = 230
-        #selection of epsilon, effects the accuracy, used in gear_tools.py
-        parameter = 0.008  
-        #background size is the size in inches of the background 
-        background_size = 5
-        
+        default_thresh = 3
+        #the number of times the frame will be dilated
+        default_dilate = 1
+
+        #Some of the properties of the GUI       
         button_size = (15, 2)
         font_size = 'Helvetica 22'
         color = ('white','#bfbfbf')
-        slider_message = "The Slider is Disabled Unless it is in Gray Scale Mode"        
+        slider_message = "The Slider is Disabled in the Normal Mode"   
+        thresh_slider_message = "Effects the Threshold \n" + slider_message
+        dilate_slider_message = "The Number of Times the Image has been Dilated \n" + slider_message     
 
         #GUI components
         control_panel = [[sg.Button('', key = 'setting', image_filename='imgs/setting_icon.png', pad=((180, 0), 0))],
-                         [sg.Button('Gray Scale', key='first_button', size=button_size, font=font_size, button_color=color, pad=(0, (10, 0)))],
+                         [sg.Button('Normal Mode', key='first_button', size=button_size, font=font_size, button_color=color, pad=(0, (10, 0)))],
                          [sg.Button('', size=(12, 1), button_color=('gray', '#EFEFEF'), pad=(0, 0), border_width=0)],
                          [sg.Button('Capture', key='second_button', size=button_size, font=font_size, button_color=color, pad=(0, 0))],
-                         [sg.Slider(range=(0, 255), key='thresh_slider', orientation='h', trough_color='#bfbfbf', size=(29, 30), default_value=default_thresh, tooltip=slider_message, pad=(0, (10, 0)), disabled=True)],
+                         [sg.Slider(range=(3, 45), key='thresh_slider', orientation='h', trough_color='#bfbfbf', size=(29, 30), default_value=default_thresh, tooltip=thresh_slider_message, pad=(0, (10, 0)))],
+                         [sg.Slider(range=(1, 5), key='dilate_slider', orientation='h', trough_color='#bfbfbf', size=(29, 30), default_value=default_dilate, tooltip=dilate_slider_message, pad=(0, (10, 0)))],
                          [sg.Multiline('Teeth# None\nDiameter# None', key='output', font=font_size, size=(16, 2), no_scrollbar=True, pad=(0, (15, 0)), disabled=True)]]
+        
         #GUI layout
         layout = [[sg.Image(filename='', key='display', background_color='gray')] +
                   [sg.Column(control_panel, vertical_alignment='top', element_justification='right')]]
@@ -63,83 +91,101 @@ class Gui:
         #GUI window
         window = sg.Window('Gear Analytics', layout, location=(200,100))    
         
-        #called before while loop so gui pops up with everthing inside without the need to load
+        #Get the first frame
         ret, frame = self.cap.read()    
         
-        #the current page that the user is on
-        page = 'home_page'
+        #The current page that the user is on
+        page = 'default_page'
 
         while True:
-            #Get the latest event from the GUI and the latest frame captured from the camera
+            #Get the latest events from the GUI and the latest frame captured from the camera
             event, values = window.Read(timeout=20)
             ret, frame = self.cap.read()
 
-            #handle the incoming events from the GUI
-            #setting button was pressed
+            #Handle the incoming events from the GUI
+            #Setting button was pressed
             if event == 'setting':
-                #create the settings window
-                default_thresh, parameter, background_size = SettingsGui.settings(default_thresh, parameter, background_size)
+                #create the settings window and get back the things that were changed
+                default_thresh, self.background_width = SettingsGui.settings(default_thresh, self.background_width)
                 window.FindElement('thresh_slider').Update(default_thresh) 
             
-            #first button was pressed
+            #First button was pressed
             elif event == 'first_button':
-                #if user is on the home page that means they want to go to the gray scale page
-                if page == 'home_page':
-                    page = 'gray_scale_page'
-                #otherwise just return the user to the home page
+                #If user is on the default page that means they want to go to the normal mode page
+                if page == 'default_page':
+                    page = 'normal_mode_page'
+                #Otherwise just return the user to the default page
                 else:
-                    page = 'home_page'
+                    page = 'default_page'
 
-                #based off the page disable and update the components accordingly
-                if page == 'home_page':
-                    window.FindElement('first_button').Update('Gray Scale')
-                    window.FindElement('thresh_slider').Update(disabled=True)                                       
+                #Based off the page disable and update the components accordingly
+                if page == 'default_page':
+                    window.FindElement('first_button').Update('Normal Mode')
+                    window.FindElement('thresh_slider').Update(disabled=False)                                       
+                    window.FindElement('dilate_slider').Update(disabled=False)                                       
 
-                elif page == 'gray_scale_page':
+                elif page == 'normal_mode_page':
                     window.FindElement('first_button').Update('Back')
-                    window.FindElement('thresh_slider').Update(disabled=False) 
+                    window.FindElement('thresh_slider').Update(disabled=True) 
+                    window.FindElement('dilate_slider').Update(disabled=True) 
 
-                #updates that are needed for both the home and gray scale pages
+                #Updates that are needed for both the default and normal mode pages
                 window.FindElement('second_button').Update(disabled=False) 
                 window.FindElement('output').Update('Teeth# None\nDiameter# None')                                      
             
-            #second button was pressed
+            #Second button was pressed
             elif event == 'second_button':
-                #user wants to get the results, hence the page is the result page
+                #User wants to get the results that means the page is the result page
                 page = 'result_page'
                 
-                #disable and update the components accordingly
+                #Disable and update the components accordingly
                 window.FindElement('first_button').Update('Back') 
                 window.FindElement('thresh_slider').Update(disabled=True)
                 window.FindElement('second_button').Update(disabled=True) 
                 
-                #run the frame through the gear class and gets the results
-                gear_result = Gear(frame, values['thresh_slider'], parameter, background_size)   
-                gear_result.color_to_thresh()
-                gear_result.find_contour()
-                gear_result.find_products()
+                #Create a Gear object to find the number of teeth on the gear
+                gear_teeth = Gear(frame, values['thresh_slider'], values['dilate_slider'], self.background_width)   
+                gear_teeth.grayscale()
+                gear_teeth.canny_edge()
+                gear_teeth.dilate()   
+                gear_teeth.threshold()
+                gear_teeth.find_teeth()
                 
+                #Create another Gear object to find the diameter of the gear
+                gear_diameter = Gear(frame, values['thresh_slider'], values['dilate_slider'], self.background_width)   
+                gear_diameter.grayscale()
+                gear_diameter.blur()
+                gear_diameter.remove_noise()
+                gear_diameter.threshold()
+                gear_diameter.find_diameter()
+
                 #write out the output to the GUI
-                output = ('Teeth# {}\nDiameter# {} in').format(gear_result.num_of_teeth, gear_result.diameter)
+                output = ('Teeth# {}\nDiameter# {} in').format(gear_teeth.num_of_teeth, gear_diameter.diameter)
                 window.FindElement('output').Update(output)
                 
-                #update the image on the GUI to hold the frame that was captured
-                imgbytes=cv2.imencode('.png', gear_result.img)[1].tobytes() 
-                window.FindElement('display').Update(data=imgbytes)   
+                #Update the image on the GUI to hold the frame that was captured
+                imgbytes=cv2.imencode('.png', gear_teeth.result)[1].tobytes() 
+                window.FindElement('display').Update(data=imgbytes)    
             
-            #if window is turned off end the program
+            #If window is turned off, then end the program
             elif event == None or event == 'Exit':
                 window.Close()
                 exit()  
             
-            #update the image on the GUI with the latest frame that was captured
-            if page == 'home_page':
+            #Update the image on the GUI with the latest frame that was captured
+            if page == 'default_page':
+                #Create a new Gear object which will convert the frame such that the user will be able to see if the gear is being captured properly 
+                convert = Gear(frame, values['thresh_slider'], values['dilate_slider'], self.background_width)
+                convert.grayscale()
+                convert.canny_edge()
+                convert.dilate()
+                convert.threshold()
+
+                #Update the GUI with the converted frame
+                imgbytes=cv2.imencode('.png', convert.frame)[1].tobytes() 
+                window.FindElement('display').Update(data=imgbytes) 
+
+            #If the page is in normal mode, then update the GUI with a normal frame
+            elif page == 'normal_mode_page':                    
                 imgbytes=cv2.imencode('.png', frame)[1].tobytes() 
                 window.FindElement('display').Update(data=imgbytes)   
-            #update the image on the GUI with the latest frame which is converted to the gray scale
-            elif page == 'gray_scale_page':                    
-                convert = Gear(frame, values['thresh_slider'], parameter, background_size)
-                convert.color_to_thresh()
-
-                imgbytes=cv2.imencode('.png', convert.threshold)[1].tobytes() 
-                window.FindElement('display').Update(data=imgbytes) 
