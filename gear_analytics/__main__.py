4ab4cdc44750007260aa59154317d76b7b81d9d3
Shahan Mudasir <shahanmny@gmail.com>
2021-06-16 15:12:32 -0400

    Added User's Arguments Parsing

--- gear_analytics/__main__.py
+++ gear_analytics/__main__.py
@@ -1,15 +1,22 @@
 import sys
+import argparse
 from gear_analytics.main_gui import Gui
 
 if __name__ == '__main__':
+    #Parse the user arguments
+    parser = argparse.ArgumentParser(description='Gear Analytics Optional Arguments')
+    
     second_camera = False
-    if(len(sys.argv) > 1):
-        second_camera = True if sys.argv[1] == '-second' else False;
+    background_width = 5
 
-    gui = Gui(second_camera)
+    parser.add_argument('-b', type=int, default=5, dest='background_width', help='The length of the background width in inches')
+    parser.add_argument('--second', action='store_true', dest='second_camera', help='Use the second camera connected to the computer')
 
+    args = parser.parse_args()
+
+    #Begin the program
+    gui = Gui(args.second_camera, args.background_width)
     gui.find_cam()
-
     gui.display_gui()
 
 
